da47b1afb12e43f5b636bbe14ae64cbd7c9b33cc
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-11-07 10:25:11 +0100

    Hoofdstuk 3 - Rechthoek

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Cirkel;
 import model.Punt;
+import model.Rechthoek;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -11,26 +12,22 @@ import model.Punt;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-
-        new Cirkel(-2, new Punt(2.5, 4.1), "fuchsia");
-        new Cirkel();
-        new Cirkel(5);
-
-        Cirkel[] mijnCirkelArray = new Cirkel[3];
-        mijnCirkelArray[0] = new Cirkel(3, new Punt(1, 4), "groen");
-        mijnCirkelArray[1] = new Cirkel();
-        mijnCirkelArray[2] = new Cirkel(6);
-        for (int arrayTeller = 0; arrayTeller < mijnCirkelArray.length;
+        System.out.println(Rechthoek.geefDefinitie());
+        Rechthoek[] mijnRechthoekArray = new Rechthoek[3];
+        mijnRechthoekArray[0] = new Rechthoek(4, 3, new Punt(2, 5), "blauw");
+        mijnRechthoekArray[1] = new Rechthoek();
+        mijnRechthoekArray[2] = new Rechthoek(25, 10);
+        for (int arrayTeller = 0; arrayTeller < mijnRechthoekArray.length;
              arrayTeller++) {
-            System.out.println(mijnCirkelArray[arrayTeller].geefOmtrek());
-            System.out.println(mijnCirkelArray[arrayTeller].geefOppervlakte());
-            System.out.println(mijnCirkelArray[arrayTeller].getMiddelpunt().getxCoordinaat());
-            System.out.printf("(%.0f, %.0f)\n",
-                    mijnCirkelArray[arrayTeller].getMiddelpunt().getxCoordinaat(),
-                    mijnCirkelArray[arrayTeller].getMiddelpunt().getyCoordinaat());
-            System.out.println();
+            System.out.println(mijnRechthoekArray[arrayTeller].geefOmtrek());
+            System.out.println(mijnRechthoekArray[arrayTeller].geefOppervlakte());
+            System.out.println(mijnRechthoekArray[arrayTeller].
+                    getHoekpuntLinksBoven().getxCoordinaat());
+            System.out.println(mijnRechthoekArray[arrayTeller].
+                    getHoekpuntLinksBoven().getyCoordinaat());
+            System.out.println(mijnRechthoekArray[arrayTeller].
+                    vertelOverGrootte());
         }
-
     }
 
 }
