b4e56969d928680669fda1c83cde97f9ef4cbb8d
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-11-07 10:12:29 +0100

    Hoofdstuk 3 - Punt gebruiken in Cirkel

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Cirkel;
+import model.Punt;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -11,18 +12,22 @@ public class MeetkundeLauncher {
 
     public static void main(String[] args) {
 
-        new Cirkel(-2, 1, 4, "fuchsia");
+        new Cirkel(-2, new Punt(2.5, 4.1), "fuchsia");
         new Cirkel();
         new Cirkel(5);
 
         Cirkel[] mijnCirkelArray = new Cirkel[3];
-        mijnCirkelArray[0] = new Cirkel(3, 1, 4, "groen");
+        mijnCirkelArray[0] = new Cirkel(3, new Punt(1, 4), "groen");
         mijnCirkelArray[1] = new Cirkel();
         mijnCirkelArray[2] = new Cirkel(6);
         for (int arrayTeller = 0; arrayTeller < mijnCirkelArray.length;
              arrayTeller++) {
             System.out.println(mijnCirkelArray[arrayTeller].geefOmtrek());
             System.out.println(mijnCirkelArray[arrayTeller].geefOppervlakte());
+            System.out.println(mijnCirkelArray[arrayTeller].getMiddelpunt().getxCoordinaat());
+            System.out.printf("(%.0f, %.0f)\n",
+                    mijnCirkelArray[arrayTeller].getMiddelpunt().getxCoordinaat(),
+                    mijnCirkelArray[arrayTeller].getMiddelpunt().getyCoordinaat());
             System.out.println();
         }
 
