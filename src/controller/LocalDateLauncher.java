79bd31e6df9d13b1214d64a6520083f560f33310
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-11-09 09:36:56 +0100

    Hoofdstuk 5.4 LocalDate

---
+++ src/controller/LocalDateLauncher.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ * <p>
+ * Laat zien hoe de LocalDate klasse werkt
+ */
+public class LocalDateLauncher {
+
+    public static void main(String[] args) {
+
+        LocalDate vandaag = LocalDate.now();
+        System.out.println(vandaag);
+
+        LocalDate beginVan2022 = LocalDate.parse("2022-01-01");
+        System.out.println(beginVan2022);
+
+        LocalDate koningsDag = LocalDate.parse("2023-04-27");
+        System.out.println(koningsDag);
+
+        if (koningsDag.isAfter(LocalDate.now())) {
+            System.out.println("Het moet nog koningsdag worden.");
+        } else if (koningsDag.equals(vandaag)) {
+            System.out.println("Het is vandaag koningsdag");
+        } else {
+            System.out.println("Het is al koningsdag geweest dit jaar.");
+        }
+
+    }
+}
