5de63ec7326e8b5cccb61fc502d4a30ece2a44e5
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2022-11-10 13:52:31 +0100

    Hoofdstuk 9 - Scanner iets korter

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -17,34 +17,27 @@ import java.util.Scanner;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-        ArrayList<String> regelsUitHetBestand = new ArrayList<>();
-
         File rechthoekenBestand = new File("resources/Rechthoek.csv");
-
+        ArrayList<Rechthoek> rechthoeken = new ArrayList<>();
         try {
             Scanner invoerBestand = new Scanner(rechthoekenBestand);
             while (invoerBestand.hasNextLine()) {
-                regelsUitHetBestand.add(invoerBestand.nextLine());
+                String[] gesplitsteRegel = invoerBestand.nextLine().split(",");
+
+                double lengte = Double.parseDouble(gesplitsteRegel[0]);
+                double breedte = Double.parseDouble(gesplitsteRegel[1]);
+
+                double xCoordinaat = Double.parseDouble(gesplitsteRegel[2]);
+                double yCoordinaat = Double.parseDouble(gesplitsteRegel[3]);
+                Punt hoekpunt = new Punt(xCoordinaat, yCoordinaat);
+
+                String kleur = gesplitsteRegel[4];
+
+                rechthoeken.add(new Rechthoek(lengte, breedte, hoekpunt, kleur));
             }
             invoerBestand.close();
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("Het lukte niet om het bestand te openen");
-        }
-
-        ArrayList<Rechthoek> rechthoeken = new ArrayList<>();
-        for (String regelUitBestand : regelsUitHetBestand) {
-            String[] gesplitsteRegel = regelUitBestand.split(",");
-
-            double lengte = Double.parseDouble(gesplitsteRegel[0]);
-            double breedte = Double.parseDouble(gesplitsteRegel[1]);
-
-            double xCoordinaat = Double.parseDouble(gesplitsteRegel[2]);
-            double yCoordinaat = Double.parseDouble(gesplitsteRegel[3]);
-            Punt hoekpunt = new Punt(xCoordinaat, yCoordinaat);
-
-            String kleur = gesplitsteRegel[4];
-
-            rechthoeken.add(new Rechthoek(lengte, breedte, hoekpunt, kleur));
         }
 
         for (Rechthoek rechthoek : rechthoeken) {
